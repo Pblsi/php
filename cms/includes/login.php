73242c960138bc234dba8a24fabba903cb7e55e9
paul blessing <paulblessing@gmail.com>
2023-02-16 15:03:15 +0100

    message

--- cms/includes/login.php
+++ cms/includes/login.php
@@ -36,7 +36,7 @@ $_SESSION['password'] =$db_password;
 $_SESSION['firstname'] =$db_user_firstname;
 $_SESSION['lastname'] =$db_user_lastname;
 $_SESSION['user_role'] =$db_user_role;
-header("Location: ../post.php");
+header("Location: ../admin");
 
 } else {
     header("Location: ../index.php");
